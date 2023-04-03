0adb9213d503df8e8547592cfaf4f3f8d91559ac
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-04-03 22:02:08 +0700

    as

--- app.js
+++ app.js
@@ -98,4 +98,8 @@ app.post("/welcome",auth,(req,res)=>{
    res.status(200).send("welcome");
 })
 
+app.get("/", auth, (req, res) => {
+   res.status(200).json({ "YO": "M6" });
+})
+
 module.exports = app;
